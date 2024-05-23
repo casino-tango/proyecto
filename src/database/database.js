b62a6bbff84d753c75a0ffa6edddddef9e0fb89f
casino-tango <unicasinosdecombiasa@gmail.com>
2024-05-23 17:08:11 -0500

    first commit

--- src/database/database.js
+++ src/database/database.js
@@ -21,7 +21,7 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize({
   dialect: 'postgres',
   host:'ep-wild-meadow-a4knsmvi-pooler.us-east-1.aws.neon.tech',
-  port: process.env.DB_PORT, // Utiliza la variable de entorno para el puerto
+  port: 5432, // Utiliza la variable de entorno para el puerto
   database: 'verceldb',
   username:'default',
   password: 'v1cjdnh2ersA',
