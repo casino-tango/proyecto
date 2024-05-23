c95d0f735ff00c0de7fa03c0fdfbbdb4303e20fe
casino-tango <unicasinosdecombiasa@gmail.com>
2024-05-23 17:18:57 -0500

    first commit32

--- src/database/database.js
+++ src/database/database.js
@@ -3,20 +3,6 @@ dotenv.config();
 import { Sequelize } from "sequelize";
 
 
-// * conxion con render*****
-//  export const sequelize = new Sequelize({
-//   database: 'casino_tango_wva4',
-//   username: 'casino_tango_wva4_user',
-//   password: 'VS2wZ7OEfSH2ix2OS6CQpyCREdOyZBDi',
-//   host: 'dpg-cojb0c6n7f5s73c68ang-a.oregon-postgres.render.com',
-//   dialect: 'postgres',
-//   dialectOptions: {
-//     ssl: true,
-//   },
-//   logging: false, // Set to true if you want to see SQL queries in logs
-// });
-
-//  *******conexion con vercel****
 
 export const sequelize = new Sequelize({
   dialect: 'postgres',
@@ -32,17 +18,5 @@ export const sequelize = new Sequelize({
     }
   }
 });
-
-
-
-
-// export const sequelize = new Sequelize(
-//   process.env.DATABASE_DATABASE,
-//   process.env.DATABASE_USERNAME,
-//   process.env.DATABASE_PASSWORD,                    
-//    {
-//     host: process.env.DATABASE_HOST,
-//   dialect: 'postgres',
-// });
 
 
