7e9d988d77a7e1ca9695edf0d966ac12b0817688
casino-tango <unicasinosdecombiasa@gmail.com>
2024-05-24 15:25:20 -0500

    prueba en redner

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json())
 import cors from 'cors'
 app.use(cors({
   // origin: 'http://localhost:3001', // Permite solicitudes solo desde este origen
-  origin: 'https://proyecto-final-de-reack.vercel.app',
+  origin: 'https://proyecto-final-de-reack.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Permite estos métodos HTTP
   allowedHeaders: ['Content-Type', 'Authorization'], // Permite estos encabezados en la solicitud
   credentials: true // Esto permite que las credenciales como las cookies se envíen con la solicitud
