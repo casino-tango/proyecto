f7f3c0a864d195779eb195f8a3f9c9ef5b37187d
casino-tango <unicasinosdecombiasa@gmail.com>
2024-05-27 09:54:00 -0500

    preuba rende13

--- src/middleware/validertoken.js
+++ src/middleware/validertoken.js
@@ -50,7 +50,7 @@ import jwt from "jsonwebtoken";
 
 export const validar2 = (req, res, next) => {
     try {
-        const {token} = req.cookies.token;
+        const token = req.cookies.token;
         if (!token) {
             console.log('No has iniciado sesión');
 
